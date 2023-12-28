06054c8bebc40b2566f72f96bb2c22f857c072fc
eshwaprasad09 <eshwarprasad707@gmail.com>
2023-12-28 20:42:33 +0530

    socialize app start

---
+++ src/test/java/com/eshwarprasad/socialize/SocializeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshwarprasad.socialize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocializeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
